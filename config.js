7f09d83c21a8a3ab40c377404e179c59775935a9
Yurko-v <uravelbovec886@gmail.com>
2026-08-21 17:33:01 +0300

    Update proxy URL to new Apps Script deployment

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Конфигурация отправки заявок
 const TELEGRAM_CONFIG = {
     // URL Google Apps Script прокси
-    PROXY_URL: 'https://script.google.com/macros/s/AKfycbzKlsJ727ovbJK66YWbA_UUql2kSfPg-LPQKWcaXHgGNxC_vUk5EhO7KKEkDePHhhd3BA/exec'
+    PROXY_URL: 'https://script.google.com/macros/s/AKfycbyy7yyOfjsVpRs1xng5Zfu2VBiI8LRVOldHnBxmWbgI0seM7xVJQnhjOYraTs6SAHOiZg/exec'
 };
