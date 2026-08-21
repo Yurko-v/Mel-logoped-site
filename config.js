cc5e99695568c06840ff85d5df51c0e0056384e6
Yurko-v <uravelbovec886@gmail.com>
2026-08-21 17:11:34 +0300

    Добавить config.js для работы формы на GitHub Pages

---
+++ config.js
@@ -0,0 +1,5 @@
+// ⚠️ Этот файл содержит секретные данные — НЕ загружайте его в Git!
+const TELEGRAM_CONFIG = {
+    BOT_TOKEN: '8876542989:AAF1ZJVSEWU-cqtKvJv-lPcQzYHG_1lj_Ls',
+    CHAT_ID: '-1003907973159'
+};
